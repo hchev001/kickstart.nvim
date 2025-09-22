4ecb223c17755f42ffba663b26d8d939d4fbc22b
Hamilton Chevez <hchev001@gmail.com>
2025-09-22 11:10:21 -0400

    various keymap changes
    changelog:
    - use jk to exit
    - leader - q to toggle quickfix list
    - ctrl + hjkl for moving between panes
    - leader we to split equally
    - leader to - new tab
    - leader tx - close current tab
    - leader tn - go to next tab
    - leader tp - go to prev tab

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,12 +1,20 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+local keymap = vim.keymap
+
+-- use jk to exit insert mode
+keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
+
+-- increment/decrement numbers
+keymap.set('n', '<leader>+', '<C-a>', { desc = 'Increment number' })
+keymap.set('n', '<leader>-', '<C-x>', { desc = 'Decrement number' })
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -14,7 +22,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -26,10 +34,10 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
@@ -38,6 +46,13 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
 -- shortcuts for splitting windows (current buffer)
-vim.keymap.set('n', '<Leader>ws', '<C-w>s', { desc = 'Split current buffer horizontally' })
-vim.keymap.set('n', '<Leader>wv', '<C-w>v', { desc = 'Split current buffer vertically' })
-vim.keymap.set('n', '<Leader>wc', '<C-w>c', { desc = 'Close the current split' })
+keymap.set('n', '<Leader>ws', '<C-w>s', { desc = 'Split current buffer horizontally' })
+keymap.set('n', '<Leader>wv', '<C-w>v', { desc = 'Split current buffer vertically' })
+keymap.set('n', '<Leader>wc', '<C-w>c', { desc = 'Close the current split' })
+keymap.set('n', '<Leader>we', '<C-w>=', { desc = 'Make splits equal size' })
+
+keymap.set('n', '<Leader>to', '<cmd>tabnew<CR>', { desc = 'Open new tab' })
+keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>', { desc = 'Close current tab' })
+keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' })
+keymap.set('n', '<leader>tp', '<cmd>tabp<CR>', { desc = 'Go to prev tab' })
+keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab' })
